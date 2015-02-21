4c573979b2d7e24b5d60a4e961f258ac72f838dc
B0cha <artbocha@gmail.com>
2015-02-21 23:03:35 +0300

    add fields in classe  Bus and Route

--- Bus.java
+++ Bus.java
@@ -1,2 +1,10 @@
 public class Bus {
+    /** Number bus */
+    private int numberBus;
+    /** Gasoline consumption in liters per kilometer */
+    private float consumptionGasoline;
+    /** name and surname of the driver */
+    private String nameSurname;
+    /** Number route */
+    private int numberRoute;
 }
--- Route.java
+++ Route.java
@@ -1,2 +1,9 @@
 public class Route {
+
+    /** Number route */
+    private int number;
+    /** Lenght route */
+    private float length;
+    /** The number of buses on the route */
+    private int amountBus;
 }
