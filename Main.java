4cc9e008b57c892c2ea91e96a637fd2215ca5b83
B0cha <artbocha@gmail.com>
2015-02-23 10:06:05 +0300

    Add ArrayLists in class main

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Route> listRoute = new ArrayList<Route>();
+        ArrayList<Bus> listBusInPark = new ArrayList<Bus>();
+        ArrayList<Bus> listBusOnRoute = new ArrayList<Bus>();
 
-
+    }
 }
