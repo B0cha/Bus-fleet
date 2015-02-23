2eafa04ab6932efaa0681eb115c5ea0a7c057bb1
B0cha <artbocha@gmail.com>
2015-02-23 13:55:23 +0300

    add method in class main 'addListBus' and in classes Bus add getters

--- Bus.java
+++ Bus.java
@@ -2,9 +2,32 @@ public class Bus {
     /** Number bus */
     private int numberBus;
     /** Gasoline consumption in liters per kilometer */
-    private float consumptionGasoline;
+    private double consumptionGasoline;
     /** name and surname of the driver */
     private String nameSurname;
     /** Number route */
     private int numberRoute;
+
+    public Bus(int numberBus, double consumptionGasoline, String nameSurname, int numberRoute) {
+        this.numberBus = numberBus;
+        this.consumptionGasoline = consumptionGasoline;
+        this.nameSurname = nameSurname;
+        this.numberRoute = numberRoute;
+    }
+
+    public int getNumberBus() {
+        return numberBus;
+    }
+
+    public double getConsumptionGasoline() {
+        return consumptionGasoline;
+    }
+
+    public String getNameSurname() {
+        return nameSurname;
+    }
+
+    public int getNumberRoute() {
+        return numberRoute;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,27 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Route> listRoute = new ArrayList<Route>();
+        ArrayList<Bus> listBus = new ArrayList<Bus>();
         ArrayList<Bus> listBusInPark = new ArrayList<Bus>();
         ArrayList<Bus> listBusOnRoute = new ArrayList<Bus>();
+    }
 
+    public static void addListBus(ArrayList<Bus> listBus) {
+
+        Scanner in = new Scanner(System.in);
+        Scanner on = new Scanner(System.in);
+        System.out.println("Введите номер автобуса");
+        int numberBus = in.nextInt();
+        System.out.println("Введите расход автобуса");
+        double consumptionGasoline = in.nextDouble();
+        System.out.println("Введите фамилию имя водителя");
+        String nameSurname = in.nextLine();
+        System.out.println("Введите имя маршрута");
+        int numberRoute = in.nextInt();
+        Bus bus = new Bus(numberBus,consumptionGasoline,nameSurname,numberRoute);
+        listBus.add(bus);
     }
 }
